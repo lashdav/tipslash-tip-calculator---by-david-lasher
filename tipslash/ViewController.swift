dc09673973b5c3d882e077b079b533be2a7fb4c1
David Lasher <dlasher@box.com>
2016-01-06 19:10:41 -0800

    Initial Commit

---
+++ tipslash/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tipslash
+//
+//  Created by David Lasher on 1/6/16.
+//  Copyright © 2016 David Lasher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
